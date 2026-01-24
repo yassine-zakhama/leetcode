42286fc00cc56660054ceaa1710a57d62009bf8c
Yassine Zakhama <yzakhama@gmail.com>
2026-01-24 23:08:11 +0100

    706

---
+++ neetcode-250/arrays-&-hashing/706.ts
@@ -0,0 +1,27 @@
+class MyHashMap {
+  private arr: number[];
+
+  constructor() {
+    this.arr = new Array(1000001).fill(-1);
+  }
+
+  put(key: number, value: number): void {
+    this.arr[key] = value;
+  }
+
+  get(key: number): number {
+    return this.arr[key];
+  }
+
+  remove(key: number): void {
+    this.arr[key] = -1;
+  }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
