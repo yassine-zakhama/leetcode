50e1ef23990a7f9404279a7fd337c3f715dc4e8b
Yassine Zakhama <yzakhama@gmail.com>
2026-01-24 22:57:10 +0100

    705

---
+++ neetcode-250/arrays-&-hashing/705.ts
@@ -0,0 +1,27 @@
+class MyHashSet {
+  private arr: boolean[];
+
+  constructor() {
+    this.arr = new Array(1000001).fill(false);
+  }
+
+  add(key: number): void {
+    this.arr[key] = true;
+  }
+
+  remove(key: number): void {
+    this.arr[key] = false;
+  }
+
+  contains(key: number): boolean {
+    return this.arr[key];
+  }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
