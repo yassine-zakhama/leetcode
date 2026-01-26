6d5d034c14ac5316e969053dcc396d3a2541510e
Yassine Zakhama <yzakhama@gmail.com>
2026-01-26 11:12:55 +0100

    35

---
+++ neetcode-250/binary-search/35-search-insert-position.ts
@@ -0,0 +1,17 @@
+function searchInsert(nums: number[], target: number): number {
+	let left = 0,
+		right = nums.length - 1;
+
+	while (left <= right) {
+		const mid = Math.trunc((left + right) / 2);
+		if (nums[mid] === target) {
+			return mid;
+		} else if (nums[mid] < target) {
+			left = mid + 1;
+		} else {
+			right = mid - 1;
+		}
+	}
+
+	return left;
+}
