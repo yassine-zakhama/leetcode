1d1214d5a4bda208302066ee3aac05a38856dd2d
Yassine Zakhama <yzakhama@gmail.com>
2026-01-28 22:58:58 +0100

    27

---
+++ neetcode-250/arrays-&-hashing/27-remove-element.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            if nums[left] == val:
+                nums[left] = nums[right]
+                right -= 1
+            else:
+                left += 1
+        return right + 1
