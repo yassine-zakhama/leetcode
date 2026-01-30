2ee01c0e5a83485e48be5f302c0abb7a11dcf1d1
Yassine Zakhama <yzakhama@gmail.com>
2026-01-30 21:06:21 +0100

    102

---
+++ neetcode-250/trees/102-binary-tree-level-order-traversal.py
@@ -0,0 +1,30 @@
+from collections import deque
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        queue, res = deque([root]), []
+        while queue:
+            level_num_nodes = len(queue)
+            level = []
+            while level_num_nodes:
+                level_num_nodes -= 1
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(level)
+        return res
