b2b20720e51fbe4653898b281c25d5a93f53d197
Yassine Zakhama <yzakhama@gmail.com>
2026-02-01 23:21:19 +0100

    1863

--- neetcode-250/backtracking/1863-sum-of-all-subset-xor-totals.py
+++ neetcode-250/backtracking/1863-sum-of-all-subset-xor-totals.py
@@ -3,9 +3,11 @@ from typing import List
 
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        def dfs(i, curr_total):
+        def do_get_total_sum(i, curr_total):
             if i == len(nums):
                 return curr_total
-            return dfs(i + 1, curr_total ^ nums[i]) + dfs(i + 1, curr_total)
+            return do_get_total_sum(i + 1, curr_total ^ nums[i]) + do_get_total_sum(
+                i + 1, curr_total
+            )
 
-        return dfs(0, 0)
+        return do_get_total_sum(0, 0)
