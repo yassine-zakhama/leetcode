36787cd7f233ce92f2946d0d8879b1aa05a4692c
Yassine Zakhama <yzakhama@gmail.com>
2026-02-01 23:21:13 +0100

    78

--- neetcode-250/backtracking/78-subsets.py
+++ neetcode-250/backtracking/78-subsets.py
@@ -5,15 +5,15 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res, curr = [], []
 
-        def dfs(i):
+        def find_subsets(i):
             if i == len(nums):
                 res.append(list(curr))
                 return
 
             curr.append(nums[i])
-            dfs(i + 1)
+            find_subsets(i + 1)
             curr.pop()
-            dfs(i + 1)
+            find_subsets(i + 1)
 
-        dfs(0)
+        find_subsets(0)
         return res
