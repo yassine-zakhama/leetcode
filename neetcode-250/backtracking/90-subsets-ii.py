56f452d762d342e7e0e3acb45f67b0a6a7610b30
Yassine Zakhama <yzakhama@gmail.com>
2026-02-02 12:28:00 +0100

    90

--- neetcode-250/backtracking/90-subsets-ii.py
+++ neetcode-250/backtracking/90-subsets-ii.py
@@ -2,22 +2,23 @@ from typing import List
 
 
 class Solution:
+    # O(n.2^n)
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        res, curr_subset = set(), []
+        res, curr_subset = [], []
 
         def do_get_subsets(i):
             if i == len(nums):
-                key = tuple(curr_subset)
-                if key not in res:
-                    res.add(key)
+                res.append(list(curr_subset))
                 return
 
             curr_subset.append(nums[i])
             do_get_subsets(i + 1)
             curr_subset.pop()
 
+            while i < len(nums) - 1 and nums[i] == nums[i + 1]:
+                i += 1
             do_get_subsets(i + 1)
 
         nums.sort()
         do_get_subsets(0)
-        return [list(s) for s in res]
+        return res
