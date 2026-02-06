e65d55f98af905098e4a7369cda0f4415d33a5ab
Yassine Zakhama <yzakhama@gmail.com>
2026-02-07 00:21:02 +0100

    746

--- neetcode-250/1d-dynamic-programming/746-min-cost-climbing-stairs.py
+++ neetcode-250/1d-dynamic-programming/746-min-cost-climbing-stairs.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        option1, option2 = cost[0], cost[1]
+        prev_cost, curr_cost = cost[0], cost[1]
         for i in range(2, len(cost)):
-            option1, option2 = option2, min(option1, option2) + cost[i]
-        return min(option1, option2)
+            prev_cost, curr_cost = curr_cost, min(prev_cost, curr_cost) + cost[i]
+        return min(prev_cost, curr_cost)
