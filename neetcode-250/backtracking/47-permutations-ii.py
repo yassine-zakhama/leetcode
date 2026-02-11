ed181f8d0b75fe716ac699b55ac6c0b5fcc81c50
Yassine Zakhama <yzakhama@gmail.com>
2026-02-11 11:32:23 +0100

    47

---
+++ neetcode-250/backtracking/47-permutations-ii.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        counts = defaultdict(int)
+        for n in nums:
+            counts[n] += 1
+
+        res, curr = [], []
+
+        def backtrack():
+            if len(curr) == len(nums):
+                res.append(curr.copy())
+                return
+
+            for n in counts:
+                if counts[n] > 0:
+                    counts[n] -= 1
+                    curr.append(n)
+                    backtrack()
+                    curr.pop()
+                    counts[n] += 1
+
+        backtrack()
+        return res
