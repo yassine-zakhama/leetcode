63dbbb13fce71cfbabb76f6d7cd647af063a684e
Yassine Zakhama <yzakhama@gmail.com>
2026-02-11 22:53:37 +0100

    91

--- neetcode-250/1d-dynamic-programming/91-decode-ways.py
+++ neetcode-250/1d-dynamic-programming/91-decode-ways.py
@@ -1,5 +1,26 @@
-# O(n) time and space
 class Solution:
+    def numDecodings(self, s: str) -> int:
+        if s[0] == "0":
+            return 0
+
+        prev2, prev1 = 1, 1
+
+        for i in range(1, len(s)):
+            curr = 0
+
+            if s[i] != "0":
+                curr += prev1
+
+            if "10" <= s[i - 1 : i + 1] <= "26":
+                curr += prev2
+
+            prev2, prev1 = prev1, curr
+
+        return prev1
+
+
+# O(n) time and space
+class Solution2:
     def numDecodings(self, s: str) -> int:
         cache = {len(s): 1}
 
