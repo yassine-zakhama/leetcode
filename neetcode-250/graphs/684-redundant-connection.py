5f5001a5d113812f25ecdfe91b2f8b467cd9c6d8
Yassine Zakhama <yzakhama@gmail.com>
2026-02-14 22:22:35 +0100

    684

--- neetcode-250/graphs/684-redundant-connection.py
+++ neetcode-250/graphs/684-redundant-connection.py
@@ -4,7 +4,7 @@ from typing import List
 # O(n) space and time
 class Solution:
     def find(self, x):
-        root = self.parent[x]
+        root = x
         while self.parent[root] != root:
             root = self.parent[root]
         while self.parent[x] != root:
@@ -12,30 +12,25 @@ class Solution:
         return root
 
     def unify(self, x, y):
-        root_x = self.find(x)
-        root_y = self.find(y)
+        root_x, root_y = self.find(x), self.find(y)
         if root_x == root_y:
             return True
 
-        if self.rank[root_x] < self.rank[root_y]:
-            self.parent[root_x] = root_y
-        elif self.rank[root_x] > self.rank[root_y]:
+        if self.rank[root_x] > self.rank[root_y]:
             self.parent[root_y] = root_x
+            self.rank[root_x] += self.rank[root_y]
         else:
-            self.parent[root_y] = root_x
-            self.rank[root_x] += 1
+            self.parent[root_x] = root_y
+            self.rank[root_y] += self.rank[root_x]
+
         return False
 
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        self.parent = {i: i for i in range(1, len(edges) + 1)}
-        self.rank = {i: 0 for i in range(1, len(edges) + 1)}
+        self.parent = [i for i in range(len(edges) + 1)]
+        self.rank = [1 for i in range(len(edges) + 1)]
 
         res = None
         for u, v in edges:
-            if u not in self.parent:
-                self.parent[u] = u
-            if v not in self.parent:
-                self.parent[v] = v
             if self.unify(u, v):
                 res = [u, v]
         return res
