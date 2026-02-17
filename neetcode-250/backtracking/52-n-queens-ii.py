86e5dc497e4e6aa3b959647315f9d5c79dcf3aab
Yassine Zakhama <yzakhama@gmail.com>
2026-02-17 01:51:32 +0100

    52

---
+++ neetcode-250/backtracking/52-n-queens-ii.py
@@ -0,0 +1,31 @@
+# O(n!) time, O(n^2) space
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        board = [["." for _ in range(n)] for _ in range(n)]
+
+        res, taken_cols, pos_diag, neg_diag = [0], set(), set(), set()
+
+        def backtrack(row):
+            if row == n:
+                res[0] += 1
+                return
+
+            for col in range(n):
+                pos, neg = row + col, row - col
+                if col in taken_cols or pos in pos_diag or neg in neg_diag:
+                    continue
+
+                board[row][col] = "Q"
+                taken_cols.add(col)
+                pos_diag.add(pos)
+                neg_diag.add(neg)
+
+                backtrack(row + 1)
+
+                neg_diag.remove(neg)
+                pos_diag.remove(pos)
+                taken_cols.remove(col)
+                board[row][col] = "."
+
+        backtrack(0)
+        return res[0]
