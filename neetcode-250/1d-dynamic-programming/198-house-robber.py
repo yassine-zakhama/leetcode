11653c350b0ee69d161ea976864634e599be9738
Yassine Zakhama <yzakhama@gmail.com>
2026-03-02 22:02:06 +0100

    198

--- neetcode-250/1d-dynamic-programming/198-house-robber.py
+++ neetcode-250/1d-dynamic-programming/198-house-robber.py
@@ -1,21 +1,6 @@
 from typing import List
 
 
-# O(n) / O(n)
-class Solution:
-    def rob(self, nums: List[int]) -> int:
-        cache = {}
-
-        def dfs(i):
-            if i >= len(nums):
-                return 0
-            if i not in cache:
-                cache[i] = nums[i] + max(dfs(i + 2), dfs(i + 3))
-            return cache[i]
-
-        return max(dfs(0), dfs(1))
-
-
 class Solution2:
     def rob(self, nums: List[int]) -> int:
         max_before_last, max_last = 0, 0
