b03dc8e1290239f6936e606b40b79f8d1ba84aff
Yassine Zakhama <yzakhama@gmail.com>
2026-03-10 03:06:31 +0100

    647

---
+++ neetcode-250/1d-dynamic-programming/647-palindromic-substrings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        def expand(left, right):
+            count = 0
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                count += 1
+                left -= 1
+                right += 1
+            return count
+
+        res = 0
+        for i in range(len(s)):
+            res += expand(i, i)
+            res += expand(i, i + 1)
+        return res
